8b4e9f5b94c1bfa96354f318655c2d17b9d2ae21
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-09 10:29:09 -0500

    Task 6 - Revenue Calculation updated

--- cc_5.js
+++ cc_5.js
@@ -23,20 +23,25 @@ order.displayOrder()
 //Task 3 - Shopping Cart.
 let cartItems = ["apple","banana","chocolate"];
 cartItems.push("Trix");
-console.log(cartItems);
+console.log(cartItems); // each console log is to show that i removed or added an item depending on the function 
 cartItems.pop();
-console.log(cartItems);
+console.log(cartItems); // same goes here pretty much to give a more valid idea
 cartItems.unshift("Pop tarts");
 cartItems.shift();
 console.log("Final list of the Cart Items:", cartItems);
 
 // Task 4 - Price Adjustments.
 let prices = [100,200,300];
-let prices_discounted = prices.map(price => price * 0.9);
+let prices_discounted = prices.map(price => price * 0.9); // get the final discounted prices of all products 
 console.log("Final Discounted Prices: ", prices_discounted)
 
 //Task 5 - Product Availability."
 let inventory = [10, 0 , 14 ,16, 18];
-let availability = inventory.filter (quantity => quantity > 0);
+let availability = inventory.filter (quantity => quantity > 0); // filter out the numbers to make sure it is greater than 0
 console.log (availability)
+
+// Task 6 - Revenue Calculation
+let sales = [500, 300, 200, 400]
+let totalRevenue = sales.reduce((sum, price) => sum + price, 0); // reduce function to calculate total revenue 
+console.log("Total revenue amounted:", totalRevenue);
 
