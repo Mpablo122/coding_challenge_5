7699df239100886815b8b8c543f96440e16c8c99
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-09 10:03:35 -0500

    Task 2 - Order Details.

--- cc_5.js
+++ cc_5.js
@@ -8,3 +8,14 @@ let customer = {
 
 console.log("Customer Database:", customer)
 
+// "Task 2 - Order Details.""
+
+let order = {
+    orderID: 12345, totalAmount: 150, status: "Processing"
+    displayOrder: function(){
+        console.log(`Order ID: ${this.orderID}`);
+        console.log(`Total amount: $${this.totalAmount}`);
+        console.log(`Status: ${this.status}`);
+    }
+};
+
