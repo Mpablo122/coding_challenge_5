5026a50702b32008d713c6ffd866b30a8b77fb03
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-09 10:11:03 -0500

    Task 3 - Shopping Cart.

--- cc_5.js
+++ cc_5.js
@@ -11,11 +11,21 @@ console.log("Customer Database:", customer)
 // "Task 2 - Order Details.""
 
 let order = {
-    orderID: 12345, totalAmount: 150, status: "Processing"
+    orderID: 12345, totalAmount: 150, status: "Processing",
     displayOrder: function(){
         console.log(`Order ID: ${this.orderID}`);
         console.log(`Total amount: $${this.totalAmount}`);
         console.log(`Status: ${this.status}`);
     }
 };
+order.displayOrder()
 
+//Task 3 - Shopping Cart.
+let cartItems = ["apple","banana","chocolate"];
+cartItems.push("Trix");
+console.log(cartItems);
+cartItems.pop();
+console.log(cartItems);
+cartItems.unshift("Pop tarts");
+cartItems.shift();
+console.log("Final list of the Cart Items:", cartItems);
